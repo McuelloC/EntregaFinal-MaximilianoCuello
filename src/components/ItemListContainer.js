bd95b7e83e112adf437ab8841148b3a7c8d426ad
Maximiliano Cuello <rmcuellocanales@gmail.com>
2023-04-26 13:04:48 -0300

    Se Agrega Firebase, Centro Nav, Pruebo video de fondo de pantalla

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
+import bgImage from "../Video/background-video.mp4"
 
 const ItemListContainer = ({ greeting }) => {
-    return (
-      <div className="container py-5">
-        <h1 className="text-center mb-4">{greeting}</h1>
-        
+  return (
+    <div className="position-relative">
+      <div className="d-none d-md-block">
+        <video autoPlay loop muted style={{ width: "100%" }}>
+          <source src={bgImage} type="video/mp4"/>
+        </video>
       </div>
-    );
-  };
+      <h1 className="text-center mb-4"
+        style={{ position: "absolute", zIndex: "1", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+        {greeting}
+      </h1>
+    </div>
+  );
+};
 
 export default ItemListContainer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar,  Nav} from 'react-bootstrap';
 import CarritoCompras from './CartWidget/CarritoCompras';
 import { Link, Outlet } from 'react-router-dom';
 
@@ -8,18 +8,18 @@ function NavBar1() {
   return (
     <>
       <Navbar className="navBg" variant='dark' expand="lg" >
-        <Container>
-          <Navbar.Brand as={Link} to="/" style={{ fontFamily: 'Love Light', fontWeight: 'bold', fontSize: 40  }}>Bona Comida</Navbar.Brand>
+      
+          <Navbar.Brand as={Link} to="/" className='bonanav' style={{ fontFamily: 'Love Light', fontWeight: 'bold', fontSize: 40  }}>Bona Comida</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav className="ms-6">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/About">About</Nav.Link>
               <Nav.Link as={Link} to="/Contact">Contacto</Nav.Link>
             </Nav>
             <CarritoCompras/>
           </Navbar.Collapse>
-        </Container>
+       
       </Navbar>
       <section>
         <Outlet></Outlet>

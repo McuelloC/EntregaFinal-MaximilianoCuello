9cb121280d7196da9235c74357a564fa66fa5291
Maximiliano Cuello <rmcuellocanales@gmail.com>
2023-05-09 20:28:29 -0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,41 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import bgVideo from "./assets/Video/background-video.mp4";
 import { DataProvider } from "./Context/DataProvider";
-import { CountProvider } from "./Context/CountProvider";
 import { Contacto, Home, Productos } from "./pages";
 
 
 
 function App() {
   return (
-   
+
     <>
-     <DataProvider>
-      <CountProvider>
-    <div className="video-background">
-    <video autoPlay muted loop>
-      <source src={bgVideo} type="video/mp4" />
-      </video>
-      </div>
-    <div >
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NavBar />}>
-            <Route path="Productos" element={<Productos />} />
-            <Route path="contact" element={<Contacto />} />
-            <Route path="/" element={<Home greeting="¡Bienvenidos a Bona Comida!" />}/>
-            <Route element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    
-  
-    </div>
-    </CountProvider> 
-    </DataProvider>
+      <DataProvider>
+       
+          <div className="video-background">
+            <video autoPlay muted loop>
+              <source src={bgVideo} type="video/mp4" />
+            </video>
+          </div>
+          <div >
+
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<NavBar />}>
+                  <Route path="Productos" element={<Productos />} />
+                  <Route path="contact" element={<Contacto />} />
+                  <Route path="/" element={<Home greeting="¡Bienvenidos a Bona Comida!" />} />
+                  <Route element={<Home />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+
+
+          </div>
+        
+      </DataProvider>
     </>
   );
-  
+
 }
 
 export default App;

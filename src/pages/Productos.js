7c62761e24f5eea012401131e04fc8bae1a3d5be
Maximiliano Cuello <rmcuellocanales@gmail.com>
2023-05-09 20:45:19 -0300

    Se agrega Drowdown en products para filtro

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,18 +1,29 @@
 
 
+import { Dropdown, DropdownButton } from "react-bootstrap"
 import ItemView from "../components/Items/ItemView"
 
-const Productos = () =>{
-    return(
+
+const Productos = () => {
+   
+    return (
         <>
-        
-        <div className="row ">
-        <ItemView/>
-        </div>
-        
+            <div className="container">
+            <DropdownButton id="dropdown-basic-button" title="Categorias">
+                <Dropdown.Item href="#/action-1" className="List">Todo</Dropdown.Item>
+                
+                <Dropdown.Item href="#/action-2" className="List">Carnes</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className="List">Pastas</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className="List">Pizzas</Dropdown.Item>
+            </DropdownButton>
+            </div>
+            <div className="row ">
+                <ItemView />
+            </div>
+
 
         </>
 
     )
 }
-export  {Productos}
+export { Productos }
